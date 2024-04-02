c58d2b1fea52efaff95c8394a93d8722def9146b
DatHo <thanhdatho0@gmail.com>
2024-04-02 15:04:48 +0700

    127.0.1 to localhost

--- src/main/java/com/ddt/mycafeteriamanagementsystem/Database.java
+++ src/main/java/com/ddt/mycafeteriamanagementsystem/Database.java
@@ -8,7 +8,7 @@ public class Database {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
 
-            Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/javafx_project", "root", "");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/javafx_project", "root", "");
             return connection;
         }catch (Exception e){e.printStackTrace();}
         return null;
